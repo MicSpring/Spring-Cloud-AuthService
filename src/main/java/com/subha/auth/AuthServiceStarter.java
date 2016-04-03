852f83a35abec992be8686b721367e397f2fb2df
MicSpring <subslg@gmail.com>
2016-04-03 16:49:31 +0530

    Spring Cloud AuthService
    The Service to be looked up and invoked by Client.
    Multiple instances of this service can be registered with Eureka

---
+++ src/main/java/com/subha/auth/AuthServiceStarter.java
@@ -0,0 +1,20 @@
+package com.subha.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+//@EnableAutoConfiguration
+@EnableDiscoveryClient
+public class AuthServiceStarter {
+	
+	public static void main(String[] args)
+	{
+		//System.setProperty("spring.config.name", "authserver-config");
+		SpringApplication.run(AuthServiceStarter.class, args);
+
+	}
+
+}
---
+++ src/main/java/com/subha/auth/controllers/AuthenticationController.java
@@ -0,0 +1,34 @@
+package com.subha.auth.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AuthenticationController {
+	
+	@Value("${db.username}")
+	String dbUser;
+	
+	@Value("${db.password}")
+	String dbPwd;
+	
+	
+	@RequestMapping("/login")
+	@ResponseBody
+	public String login()
+	{
+		return "Hurrahh!!! User Logged In....";
+	}
+
+	@RequestMapping("/loginUser")
+	@ResponseBody
+	public String loginUser(@RequestBody String user)
+	{
+		System.out.println("USERNAME:"+dbUser+"\n\n PASSWORD:"+dbPwd);
+		return "Hurrahh!!! "+user+" Logged In....";
+	}
+
+}
