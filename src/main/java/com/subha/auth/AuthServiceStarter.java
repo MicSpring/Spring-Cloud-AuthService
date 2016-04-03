25eab46f3be2221c667f44bb7ed9372ebeca6fac
MicSpring <subslg@gmail.com>
2016-04-04 01:01:19 +0530

    Spring Could AUTH-SERVICE
    Changes for Hystrix

--- src/main/java/com/subha/auth/AuthServiceStarter.java
+++ src/main/java/com/subha/auth/AuthServiceStarter.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 //@EnableAutoConfiguration
 @EnableDiscoveryClient
+//@EnableHystrix
 public class AuthServiceStarter {
 	
 	public static void main(String[] args)
--- src/main/java/com/subha/auth/controllers/AuthenticationController.java
+++ src/main/java/com/subha/auth/controllers/AuthenticationController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @Controller
 public class AuthenticationController {
 	
@@ -23,12 +25,20 @@ public class AuthenticationController {
 		return "Hurrahh!!! User Logged In....";
 	}
 
+	//@HystrixCommand(fallbackMethod="loginUserFallback")
 	@RequestMapping("/loginUser")
 	@ResponseBody
 	public String loginUser(@RequestBody String user)
 	{
 		System.out.println("USERNAME:"+dbUser+"\n\n PASSWORD:"+dbPwd);
 		return "Hurrahh!!! "+user+" Logged In....";
+	}
+	
+	
+	public String loginUserFallback( String user)
+	{
+		System.out.println("USERNAME:"+dbUser+"\n\n PASSWORD:"+dbPwd);
+		return "Ooops!!! "+user+" is in FallBack....";
 	}
 
 }
